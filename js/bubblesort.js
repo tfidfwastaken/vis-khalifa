5d6c453c87724f7b26830bcacbaafef3cd2142c6
Atharva Raykar <raykar.ath@gmail.com>
2019-11-12 22:44:22 +0530

    Add buggy live demo

--- js/bubblesort.js
+++ js/bubblesort.js
@@ -1,23 +1,34 @@
+var btn = document.getElementById('load_values');
 var av = new JSAV("container");
+var inputarr = [];
+var arr = null;
 
-var arr = av.ds.array([5, 6, 1, 3, 2], {indexed: true});
-av.displayInit();
-var i = 0;
-var j = 0;
-for(i = 0; i < arr.size(); i++) {
-    av.step();
-    for(j = 0; j < arr.size()-i-1; j++) {
-        av.umsg("i = {i} <br> j = {j}", {fill: {j: j, i: i}});
-        arr.highlight([j, j + 1]);
-        av.step();
-        if(arr.value(j) > arr.value(j+1)) {
-            av.umsg("j is greater than j+1, so swapping");
-            av.step();
-            arr.swap(j, j + 1);
-            av.step();
-        }
-        arr.unhighlight([j, j + 1]);
+btn.onclick = startVis;
+
+function startVis(e) {
+    inputarr = document.getElementById('arrayinput').value.split(' ').map(Number);
+    if(arr) {
+        arr.clear();
+        av.clearumsg();
     }
+    console.log(inputarr);
+    arr = av.ds.array(inputarr, {indexed: true});
+    var i, j;
+    av.displayInit();
+    for(i = 0; i < arr.size(); i++) {
+        av.step();
+        for(j = 0; j < arr.size()-i-1; j++) {
+            av.umsg("i = {i} <br> j = {j}", {fill: {j: j, i: i}});
+            arr.unhighlight(true);
+            arr.highlight([j, j + 1]);
+            av.step();
+            if(arr.value(j) > arr.value(j+1)) {
+                av.umsg("j is greater than j+1, so swapping");
+                av.step();
+                arr.swap(j, j + 1);
+                av.step();
+            }
+        }
+    }
+    av.recorded();
 }
-
-av.recorded();
