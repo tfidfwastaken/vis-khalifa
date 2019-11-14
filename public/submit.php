f3e53fcda37c173c48c77acb6af48250faca8a5f
Gituser143 <bhargavsnv100@gmail.com>
2019-11-14 20:19:03 +0530

    Made SQL Backend

--- public/submit.php
+++ public/submit.php
@@ -1,8 +1,28 @@
 <html>
     <body>
         <?php
-        $var = $_POST["newsletterName"];
-        echo $var;
+        $var1 = $_POST["newsletterName"];
+        $var2 = $_POST["newsletterEmail"];
+        $servername = "localhost";
+	$username = "root";
+	$password = "password";
+	$dbname = "algo";
+	$conn = new mysqli($servername,$username,$password,$dbname);
+	if($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "INSERT INTO users (name,email) VALUES ('$var1' , '$var2');";
+	if ($conn->query($sql) === TRUE) 
+	{
+  		echo "New record created successfully";
+	}
+	else 
+	{
+  		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn->close();
+	header('Location: index.html');
         ?>
     </body>
 </html>
