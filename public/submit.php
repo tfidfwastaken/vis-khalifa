a24c5029613d0f07d7ce6444cdf84e609106312b
Siddhant <raosiddhant99@gmail.com>
2019-11-17 19:31:44 +0530

    added submit:

--- public/submit.php
+++ public/submit.php
@@ -1,10 +1,22 @@
 <html>
     <body>
         <?php
-        $var1 = $_POST["newsletterName"];
-        $var2 = $_POST["newsletterEmail"];
-        echo $var1, $var2;
-        header('Location: index.html');
+	        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	        	if (!empty($_POST["newsletterName"]) && !empty($_POST["newsletterEmail"])) {
+					$name = $_POST["newsletterName"];
+		        	$email = $_POST["newsletterEmail"];
+		        	//regex to valiadte email
+					if (!preg_match("/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/", $email)) {
+ 						exit('Invalid email format');
+					}
+		        } else {
+		        	exit('Name or Email cannot be empty');
+		        }
+			} else {
+				exit('Invalid Request');
+			} 
+			//validation successful!
+			header('Location: index.html');
         ?>
     </body>
 </html>
