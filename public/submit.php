8536f081750e6f860b4419e0486643c191aa2af6
Siddhant <raosiddhant99@gmail.com>
2019-11-18 09:10:50 +0530

    Handle php error

--- public/submit.php
+++ public/submit.php
@@ -1,19 +1,25 @@
 <html>
     <body>
         <?php
+        	$timeout=0;
+		    $url="newsletter.html";
 	        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	        	if (!empty($_POST["newsletterName"]) && !empty($_POST["newsletterEmail"])) {
 					$name = $_POST["newsletterName"];
 		        	$email = $_POST["newsletterEmail"];
+		        	
 		        	//regex to valiadte email
 					if (!preg_match("/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/", $email)) {
- 						exit('Invalid email format');
+ 						echo "<script type='text/javascript'>alert('Invalid Email Format');</script>";
+ 						echo "<meta http-equiv='refresh' content='$timeout;$url' />";
 					}
 		        } else {
-		        	exit('Name or Email cannot be empty');
+ 					echo "<script type='text/javascript'>alert('Name or Email cannot be empty');</script>";
+ 					echo "<meta http-equiv='refresh' content='$timeout;$url' />";
 		        }
 			} else {
-				exit('Invalid Request');
+ 				echo "<script type='text/javascript'>alert('Invalid Request');</script>";
+ 				echo "<meta http-equiv='refresh' content='$timeout;$url' />";
 			} 
 			//validation successful!
 	        $servername = "localhost";
@@ -23,19 +29,20 @@
 			$conn = new mysqli($servername,$username,$password,$dbname);
 			if($conn->connect_error)
 			{
-				die("Connection failed: " . $conn->connect_error);
+				echo "<script type='text/javascript'>alert('Connection failed');</script>";
+ 				echo "<meta http-equiv='refresh' content='$timeout;$url' />";
 			}
 			$sql = "INSERT INTO users (name,email) VALUES ('$var1' , '$var2');";
 			if ($conn->query($sql) === TRUE) 
 			{
-		  		echo "New record created successfully";
+				echo "<script type='text/javascript'>alert('Thank you for signing up!');</script>";
+ 				echo "<meta http-equiv='refresh' content='$timeout;$url' />";
 			}
 			else 
 			{
-		  		echo "Error: " . $sql . "<br>" . $conn->error;
+ 				echo "<meta http-equiv='refresh' content='$timeout;$url' />";
 			}
 			$conn->close();
-			header('Location: index.html');
         ?>
     </body>
 </html>
