dd61b4a82aa614d357eff4c019789567c7b3e223
Siddhant <raosiddhant99@gmail.com>
2019-11-18 08:17:09 +0530

    add email validation

--- public/submit.php
+++ public/submit.php
@@ -16,6 +16,25 @@
 				exit('Invalid Request');
 			} 
 			//validation successful!
+	        $servername = "localhost";
+			$username = "root";
+			$password = "password";
+			$dbname = "algo";
+			$conn = new mysqli($servername,$username,$password,$dbname);
+			if($conn->connect_error)
+			{
+				die("Connection failed: " . $conn->connect_error);
+			}
+			$sql = "INSERT INTO users (name,email) VALUES ('$var1' , '$var2');";
+			if ($conn->query($sql) === TRUE) 
+			{
+		  		echo "New record created successfully";
+			}
+			else 
+			{
+		  		echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+			$conn->close();
 			header('Location: index.html');
         ?>
     </body>
